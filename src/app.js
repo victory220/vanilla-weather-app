78d7a626e72642d66c66c48218a937353d301c83
victory220 <107876583+victory220@users.noreply.github.com>
2022-06-29 19:11:25 +0300

    Added weather API and date

---
+++ src/app.js
@@ -0,0 +1,45 @@
+function addZeroToTime(str) {
+  if (str < 10) {
+    return `0${str}`;
+  } else {
+    return str;
+  }
+}
+
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = addZeroToTime(date.getHours());
+  let minutes = addZeroToTime(date.getMinutes());
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
+function showWeather(response) {
+  console.log(response);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+}
+
+let city = "London";
+let apiKey = "69a8934df3c12df0dc3ffddf1977ee44";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+axios.get(apiUrl).then(showWeather);
