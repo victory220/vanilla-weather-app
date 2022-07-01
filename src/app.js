1f2db89b306d32606da3c1d8214e31f7b1c590ff
victory220 <107876583+victory220@users.noreply.github.com>
2022-07-01 21:16:04 +0300

    Created a row of forecast elements in JS

--- src/app.js
+++ src/app.js
@@ -72,6 +72,27 @@ function displayCelsiusTemperature(event) {
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
+function displayForecast() {
+  let forecastElement = document.querySelector("#weather-forecast");
+  let forecastHTML = `<div class="row">`;
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+<div class="col-2">
+  <div class="forecast-date">${day}</div>
+  <img src="http://openweathermap.org/img/wn/04d@2x.png" alt="" width="42" />
+  <div class="forecast-temperatures">
+    <span class="temperature-max"> 19° </span>
+    <span class="temperature-min"> 12° </span>
+  </div>
+</div>
+  `;
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
 
 let celsiusTemperature = null;
 
@@ -83,3 +104,4 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("London");
+displayForecast();
